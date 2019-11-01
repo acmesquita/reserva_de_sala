0964a8d17fc9d6284dbc4dd6d9425a8c1e167032
Catharina Mesquita <catha.ana.1994@gmail.com>
2019-10-31 21:22:27 -0300

    Criando teste para usuário

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "criando um usuário vazio e validando" do
+    user = User.create({})
+    assert_not user.valid?
+  end
+
+  test "criando um usuário somente com name" do
+    user = User.create({name: "João Bobo"})
+    assert_not user.valid?
+  end
+
+  test "criando um usuário valido" do
+    user = User.create({name: "João Bobo", email: "joao@test.com", password: "12345678"})
+    assert user.valid?
+  end
+
+
+  test "criando dois usuários iguais" do
+    user1 = User.create({name: "João Bobo", email: "joao@test.com", password: "12345678"})
+    user2 = User.create({name: "João Bobo", email: "joao@test.com", password: "12345678"})
+
+    assert user1.valid?
+    assert_not user2.valid?
+    assert_not_empty user2.errors
+    assert user2.errors[:email], "já está em uso"
+  end
 end
