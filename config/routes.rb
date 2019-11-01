efb1fd73d8528fea53ce84b33ae368c121ac7924
Catharina Mesquita <catha.ana.1994@gmail.com>
2019-10-31 23:13:09 -0300

    Aplicando bootstrap e personalisando css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root 'user#sing_up'
+  authenticated :user do
+    root to: 'welcome#index', as: :authenticated_root
+  end
+  root to: redirect('/users/sign_in')
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
