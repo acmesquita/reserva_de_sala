a679f9d9f5e3416c0ca556f80d3fba2c1d6d455c
Catharina Mesquita <catha.ana.1994@gmail.com>
2019-11-01 22:07:23 -0300

    Criando reserva

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,37 @@
 class ReservationsController < ApplicationController
+  
+  before_action :set_reserve, only: [:edit, :show]
+
   def index
+    @reservations = Reserve.where("date_start >= ?", Time.zone.now)
+  end
+
+  def show
   end
 
   def new
+    @reserve = Reserve.new()
   end
 
   def edit
+  end
+
+  def create
+    @reserve = Reserve.new(reserve_params.merge({user: current_user}))
+    if @reserve.save
+      redirect_to reservations_path, notice: "Reserva criada com sucesso!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_reserve
+    @reserve = Reserve.find(params[:id])
+  end
+
+  def reserve_params
+    params.require(:reserve).permit(:title, :description, :date_start, :date_end)
   end
 end
--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,2 +1,6 @@
 module ReservationsHelper
+
+  def format_time(reserve)
+    "#{l(reserve.date_start)} às #{reserve.date_start.strftime("%H:%M")} - #{reserve.date_end.strftime("%H:%M")}"
+  end
 end
